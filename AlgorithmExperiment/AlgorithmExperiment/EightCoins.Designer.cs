8b1f2b71c372e61d9c0f11dd545507641cba9325
Harvest <cf951016@qq.com>
2015-12-16 20:50:43 +0800

    八枚硬币问题完成，但没有算法改进

--- AlgorithmExperiment/AlgorithmExperiment/EightCoins.Designer.cs
+++ AlgorithmExperiment/AlgorithmExperiment/EightCoins.Designer.cs
@@ -28,24 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listRst = new System.Windows.Forms.ListBox();
+            this.btnCreateSequence = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClr = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listRst
+            // 
+            this.listRst.Font = new System.Drawing.Font("宋体", 11F);
+            this.listRst.FormattingEnabled = true;
+            this.listRst.ItemHeight = 15;
+            this.listRst.Location = new System.Drawing.Point(123, 12);
+            this.listRst.Name = "listRst";
+            this.listRst.Size = new System.Drawing.Size(373, 259);
+            this.listRst.TabIndex = 0;
+            // 
+            // btnCreateSequence
+            // 
+            this.btnCreateSequence.Font = new System.Drawing.Font("宋体", 11F);
+            this.btnCreateSequence.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateSequence.Name = "btnCreateSequence";
+            this.btnCreateSequence.Size = new System.Drawing.Size(93, 52);
+            this.btnCreateSequence.TabIndex = 1;
+            this.btnCreateSequence.Text = "创建硬币序列";
+            this.btnCreateSequence.UseVisualStyleBackColor = true;
+            this.btnCreateSequence.Click += new System.EventHandler(this.btnCreateSequence_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("宋体", 11F);
+            this.btnStart.Location = new System.Drawing.Point(12, 70);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(93, 52);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "开始执行";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClr
+            // 
+            this.btnClr.Font = new System.Drawing.Font("宋体", 11F);
+            this.btnClr.Location = new System.Drawing.Point(12, 128);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(93, 52);
+            this.btnClr.TabIndex = 1;
+            this.btnClr.Text = "清屏";
+            this.btnClr.UseVisualStyleBackColor = true;
+            this.btnClr.Click += new System.EventHandler(this.btnClr_Click);
             // 
             // EightCoins
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(508, 284);
+            this.Controls.Add(this.btnClr);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnCreateSequence);
+            this.Controls.Add(this.listRst);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "EightCoins";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "EightCoins";
+            this.Text = "减治法在组合问题中的应用——8枚硬币问题";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EightCoins_FormClosing);
+            this.Load += new System.EventHandler(this.EightCoins_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listRst;
+        private System.Windows.Forms.Button btnCreateSequence;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClr;
     }
 }
--- AlgorithmExperiment/AlgorithmExperiment/EightCoins.cs
+++ AlgorithmExperiment/AlgorithmExperiment/EightCoins.cs
@@ -12,6 +12,8 @@ namespace AlgorithmExperiment
 {
     public partial class EightCoins : Form
     {
+        int[] eightCoins = new int[8];
+
         Form mainF;
         public EightCoins(Form f)
         {
@@ -31,6 +33,182 @@ namespace AlgorithmExperiment
                 e.Cancel = false;
                 mainF.Show();
             }
+        }
+
+        #region EightCoinsAlgorithms
+        public string CreateASequence()
+        {
+            Random random = new Random();
+            int fateCoinIndex = random.Next(0, 7);
+            int fateCoinWeight = random.Next(0, 10);
+            int trueCoinWeight = random.Next(0, 10);
+            while (true)
+            {
+                if (trueCoinWeight == fateCoinWeight)
+                {
+                    trueCoinWeight = random.Next(0, 10);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                if (i == fateCoinIndex)
+                {
+                    eightCoins[i] = fateCoinWeight;
+                }
+                else
+                {
+                    eightCoins[i] = trueCoinWeight;
+                }
+            }
+            if (fateCoinWeight > trueCoinWeight)
+            {
+                return string.Format("第{0}枚硬币是比较重的假币，重量为{1}，真币重量为{2}", (fateCoinIndex + 1).ToString(), fateCoinWeight.ToString(), trueCoinWeight.ToString());
+            }
+            else
+            {
+                return string.Format("第{0}枚硬币是比较轻的假币，重量为{1}，真币重量为{2}", (fateCoinIndex + 1).ToString(), fateCoinWeight.ToString(), trueCoinWeight.ToString());
+            }
+        }
+
+        public void EightCoinsAlgorithm()
+        {
+            listRst.Items.Add("-------------Comparison Start-------------");
+            listRst.Items.Add("C1+C2+C3 ? C4+C5+C6");
+            if (eightCoins[0] + eightCoins[1] + eightCoins[2] > eightCoins[3] + eightCoins[4] + eightCoins[5])
+            {
+                listRst.Items.Add("C1+C2+C3 > C4+C5+C6, C1+C4 ? C2+C5");
+                if (eightCoins[0] + eightCoins[3] > eightCoins[1] + eightCoins[4])
+                {
+                    listRst.Items.Add("C1+C4 > C2+C5, C1 ? C2");
+                    if (eightCoins[0] > eightCoins[1])
+                    {
+                        listRst.Items.Add("C1 > C2, C1 is a HEAVIER FATE coin.");
+                    }
+                    else
+                    {
+                        listRst.Items.Add("C1 = C2, C5 is a LIGHTER FATE coin.");
+                    }
+                }
+                else if (eightCoins[0] + eightCoins[3] < eightCoins[1] + eightCoins[4])
+                {
+                    listRst.Items.Add("C1+C4 < C2+C5, C2 ? C1");
+                    if (eightCoins[1] > eightCoins[0])
+                    {
+                        listRst.Items.Add("C2 > C1, C2 is a HEAVIER FATE coin.");
+                    }
+                    else
+                    {
+                        listRst.Items.Add("C2 = C1, C3 is a LIGHTER FATE coin.");
+                    }
+                }
+                else
+                {
+                    listRst.Items.Add("C1+C4 = C2+C5, C3 ? C1");
+                    if (eightCoins[2] > eightCoins[0])
+                    {
+                        listRst.Items.Add("C3 > C1, C5 is a HEAVIER FATE coin.");
+                    }
+                    else
+                    {
+                        listRst.Items.Add("C3 = C1, C6 is a LIGHTER FATE coin.");
+                    }
+                }
+            }
+            else if (eightCoins[0] + eightCoins[1] + eightCoins[2] < eightCoins[3] + eightCoins[4] + eightCoins[5])
+            {
+                listRst.Items.Add("C1+C2+C3 < C4+C5+C6, C1+C4 ? C2+C5");
+                if (eightCoins[0] + eightCoins[3] > eightCoins[1] + eightCoins[4])
+                {
+                    listRst.Items.Add("C1+C4 > C2+C5, C1 ? C2");
+                    if (eightCoins[0] > eightCoins[1])
+                    {
+                        listRst.Items.Add("C1 > C2, C2 is a LIGHTER FATE coin.");
+                    }
+                    else
+                    {
+                        listRst.Items.Add("C1 = C2, C4 is a HEAVIER FATE coin.");
+                    }
+                }
+                else if (eightCoins[0] + eightCoins[3] < eightCoins[1] + eightCoins[4])
+                {
+                    listRst.Items.Add("C1+C4 < C2+C5, C2 ? C1");
+                    if (eightCoins[1] > eightCoins[0])
+                    {
+                        listRst.Items.Add("C2 > C1, C1 is a LIGHTER FATE coin.");
+                    }
+                    else
+                    {
+                        listRst.Items.Add("C2 = C1, C5 is a HEAVIER FATE coin.");
+                    }
+                }
+                else
+                {
+                    listRst.Items.Add("C1+C4 = C2+C5, C1 ? C3");
+                    if (eightCoins[0] > eightCoins[2])
+                    {
+                        listRst.Items.Add("C1 > C3, C3 is a LIGHTER FATE coin.");
+                    }
+                    else
+                    {
+                        listRst.Items.Add("C1 = C3, C6 is a HEAVIER FATE coin.");
+                    }
+                }
+            }
+            else
+            {
+                listRst.Items.Add("C1+C2+C3 = C4+C5+C6, C7 ? C8");
+                if (eightCoins[6] > eightCoins[7])
+                {
+                    listRst.Items.Add("C7 > C8, C7 ? C1");
+                    if (eightCoins[6] > eightCoins[0])
+                    {
+                        listRst.Items.Add("C7 > C1, C7 is a HEAVIER FATE coin.");
+                    }
+                    else
+                    {
+                        listRst.Items.Add("C7 = C1, C8 is a LIGHTER FATE coin.");
+                    }
+                }
+                else
+                {
+                    listRst.Items.Add("C7 < C8, C8 ? C1");
+                    if (eightCoins[7] > eightCoins[0])
+                    {
+                        listRst.Items.Add("C8 > C1, C8 is a HEAVIER FATE coin.");
+                    }
+                    else
+                    {
+                        listRst.Items.Add("C8 = C1, C7 is a LIGHTER FATE coin.");
+                    }
+                }
+            }
+            listRst.Items.Add("-------------Comparison   End-------------");
+        }
+        #endregion
+
+        private void EightCoins_Load(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+        }
+
+        private void btnCreateSequence_Click(object sender, EventArgs e)
+        {
+            listRst.Items.Add(CreateASequence());
+            btnStart.Enabled = true;
+        }
+
+        private void btnClr_Click(object sender, EventArgs e)
+        {
+            listRst.Items.Clear();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            EightCoinsAlgorithm();
         }
     }
 }
