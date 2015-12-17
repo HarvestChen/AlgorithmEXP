028f3a46ac852cc6a75367023ab70fd04753bb89
Harvest_Dorm <cf951016@qq.com>
2015-12-17 14:45:32 +0800

    堆排序完成

--- AlgorithmExperiment/AlgorithmExperiment/HeapSort.Designer.cs
+++ AlgorithmExperiment/AlgorithmExperiment/HeapSort.Designer.cs
@@ -28,24 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.unsortedArea = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sortedArea = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timeEllapsedLabel = new System.Windows.Forms.Label();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnCls = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataNumber = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataNumber)).BeginInit();
             this.SuspendLayout();
+            // 
+            // unsortedArea
+            // 
+            this.unsortedArea.Location = new System.Drawing.Point(112, 13);
+            this.unsortedArea.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.unsortedArea.Name = "unsortedArea";
+            this.unsortedArea.Size = new System.Drawing.Size(681, 129);
+            this.unsortedArea.TabIndex = 0;
+            this.unsortedArea.TabStop = false;
+            this.unsortedArea.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "待排序区域";
+            // 
+            // sortedArea
+            // 
+            this.sortedArea.Location = new System.Drawing.Point(112, 150);
+            this.sortedArea.Margin = new System.Windows.Forms.Padding(4);
+            this.sortedArea.Name = "sortedArea";
+            this.sortedArea.Size = new System.Drawing.Size(681, 129);
+            this.sortedArea.TabIndex = 0;
+            this.sortedArea.TabStop = false;
+            this.sortedArea.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "有序区域";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "排序耗时";
+            // 
+            // timeEllapsedLabel
+            // 
+            this.timeEllapsedLabel.AutoSize = true;
+            this.timeEllapsedLabel.Location = new System.Drawing.Point(91, 302);
+            this.timeEllapsedLabel.Name = "timeEllapsedLabel";
+            this.timeEllapsedLabel.Size = new System.Drawing.Size(72, 16);
+            this.timeEllapsedLabel.TabIndex = 1;
+            this.timeEllapsedLabel.Text = "排序耗时";
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(589, 293);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(99, 34);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "生成数据";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnCls
+            // 
+            this.btnCls.Location = new System.Drawing.Point(694, 293);
+            this.btnCls.Name = "btnCls";
+            this.btnCls.Size = new System.Drawing.Size(99, 34);
+            this.btnCls.TabIndex = 2;
+            this.btnCls.TabStop = false;
+            this.btnCls.Text = "清屏";
+            this.btnCls.UseVisualStyleBackColor = true;
+            this.btnCls.Click += new System.EventHandler(this.btnCls_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(484, 293);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(99, 34);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "开始排序";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(219, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "随机生成";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(375, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "个数(3~5000)";
+            // 
+            // dataNumber
+            // 
+            this.dataNumber.Location = new System.Drawing.Point(297, 297);
+            this.dataNumber.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.dataNumber.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.dataNumber.Name = "dataNumber";
+            this.dataNumber.Size = new System.Drawing.Size(72, 26);
+            this.dataNumber.TabIndex = 3;
+            this.dataNumber.TabStop = false;
+            this.dataNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.dataNumber.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             // 
             // HeapSort
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(806, 335);
+            this.Controls.Add(this.dataNumber);
+            this.Controls.Add(this.btnCls);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.timeEllapsedLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sortedArea);
+            this.Controls.Add(this.unsortedArea);
+            this.Font = new System.Drawing.Font("宋体", 12F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "HeapSort";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "HeapSort";
+            this.Text = "变治法在排序问题中的应用——堆排序问题";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.HeapSort_FormClosing);
+            this.Load += new System.EventHandler(this.HeapSort_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataNumber)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox unsortedArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox sortedArea;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label timeEllapsedLabel;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnCls;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown dataNumber;
     }
 }
--- AlgorithmExperiment/AlgorithmExperiment/HeapSort.cs
+++ AlgorithmExperiment/AlgorithmExperiment/HeapSort.cs
@@ -32,5 +32,197 @@ namespace AlgorithmExperiment
                 mainF.Show();
             }
         }
+
+        #region HeapSortAlgorithms
+        /// <summary>
+        /// 元素交换
+        /// </summary>
+        /// <param name="a">元素a</param>
+        /// <param name="b">元素b</param>
+        public void Swap(ref int a, ref int b)
+        {
+            int temp = 0;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// 大顶堆的调整过程
+        /// </summary>
+        /// <param name="array">待调整的数组</param>
+        /// <param name="currentIndex">待调整元素在数组中的位置(根节点)</param>
+        /// <param name="heapSize">堆中所有元素的个数</param>
+        public void MaxHeapify(int[] array, int currentIndex, int heapSize)
+        {
+            try
+            {
+                //左子节点在数组中的位置
+                int left = 2 * currentIndex + 1;
+                //右子节点在数组中的位置
+                int right = 2 * currentIndex + 2;
+                //记录此根节点, 左子节点, 右子节点三者中最大值的位置
+                int large = currentIndex;
+
+                //与左子节点进行比较
+                if (left < heapSize && array[left] > array[large])
+                {
+                    large = left;
+                }
+                //与右子节点进行比较
+                if (right < heapSize && array[right] > array[large])
+                {
+                    large = right;
+                }
+                //如果 currentIndex != large 则表明 large 发生变化(左右子节点中有大于根节点的情况)
+                if (currentIndex != large)
+                {
+                    //将左右节点中的大者与根节点进行交换(实现局部大顶堆)
+                    Swap(ref array[currentIndex], ref array[large]);
+                    //以上次调整动作的large位置(为此次调整的根节点位置), 进行递归调整
+                    MaxHeapify(array, large, heapSize);
+                }
+            }
+            catch(Exception ex) { }
+        }
+
+        /// <summary>
+        /// 创建大顶堆(根节点大于左右子节点)
+        /// </summary>
+        /// <param name="array">待排序数组</param>
+        public void BuildMaxHeap(int[] array)
+        {
+            try
+            {
+                //根据大顶堆的性质可知: 数组的前半段元素为根节点, 其余元素都为叶子节点
+                //从最底层的最后一个根节点开始进行大顶堆的调整
+                for(int i = array.Length / 2 - 1; i >= 0; i--)
+                {
+                    //调整大顶堆
+                    MaxHeapify(array, i, array.Length);
+                }
+            }
+            catch(Exception ex) { }
+        }
+
+        /// <summary>
+        /// 堆排序算法
+        /// </summary>
+        /// <param name="array">待排序数组</param>
+        public void HeapSortAlgorithm(int[] array)
+        {
+            try
+            {
+                //创建大顶堆
+                BuildMaxHeap(array);
+                for(int i = array.Length - 1; i > 0; i--)
+                {
+                    //将堆顶元素依次与无序区的最后一位交换(使堆顶元素进入有序区)
+                    Swap(ref array[0], ref array[i]);
+                    //重新将无序区调整为大顶堆
+                    MaxHeapify(array, 0, i);
+                }
+            }
+            catch(Exception ex) { }
+        }
+        #endregion
+
+        private void HeapSort_Load(object sender, EventArgs e)
+        {
+            timeEllapsedLabel.Text = "";
+            sortedArea.Text = "";
+            unsortedArea.Text = "";
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (unsortedArea.Text.Trim(' ') != "")
+            {
+                try
+                {
+                    timeEllapsedLabel.Text = "";
+                    sortedArea.Text = "";
+                    System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+                    string dataRead = this.unsortedArea.Text;
+                    int[] array;
+                    string[] b;
+                    string[] a = dataRead.Split(' ');
+                    if (a[a.Length - 1] == "")
+                    {
+                        b = new string[a.Length - 1];
+                        for (int i = 0; i < b.Length; i++)
+                        {
+                            b[i] = a[i];
+                        }
+                        array = new int[b.Length];
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            array[i] = Convert.ToInt32(b[i]);
+                        }
+                        watch.Reset();
+                        watch.Start();
+                        HeapSortAlgorithm(array);
+                        watch.Stop();
+                        timeEllapsedLabel.Text = watch.ElapsedMilliseconds.ToString() + "毫秒";
+                        foreach (int k in array)
+                        {
+                            sortedArea.Text += k.ToString() + " ";
+                        }
+                    }
+                    else
+                    {
+                        array = new int[a.Length];
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            array[i] = Convert.ToInt32(a[i]);
+                        }
+                        watch.Reset();
+                        watch.Start();
+                        HeapSortAlgorithm(array);
+                        watch.Stop();
+                        timeEllapsedLabel.Text = watch.ElapsedMilliseconds.ToString() + "毫秒";
+                        foreach (int k in array)
+                        {
+                            sortedArea.Text += k.ToString() + " ";
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("请生成数据或手动输入数据，数与数之间使用空格做间隔。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                unsortedArea.Text = "";
+                sortedArea.Text = "";
+                timeEllapsedLabel.Text = "";
+                int num = (int)this.dataNumber.Value;
+                Random random = new Random();
+                for (int i = 0; i < num; i++)
+                {
+                    unsortedArea.Text += random.Next().ToString() + " ";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCls_Click(object sender, EventArgs e)
+        {
+            unsortedArea.Text = "";
+            sortedArea.Text = "";
+            timeEllapsedLabel.Text = "";
+        }
     }
 }
